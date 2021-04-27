628126355a1cdb9dcddff6240fc52b8dc06560fc
Marcos Daniel <marcosnumer11@gmail.com>
2021-04-27 17:19:07 -0300

    Aula3 completed

---
+++ src/components/Header.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import {
+    Image,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native'
+import colors from '../styles/colors'
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
+
+import userImg from '../assets/eggman.jpg'
+import fonts from '../styles/fonts'
+
+export function Header() {
+    return (
+        <View style={styles.container}>
+            <View>
+                <Text style={styles.greeting}>
+                    Olá,
+                </Text>
+                <Text style={styles.userName}>
+                    Egg man
+                </Text>
+            </View>
+            <Image
+                source={userImg}
+                style={styles.image} />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingVertical: 20,
+        marginTop: getStatusBarHeight(),
+    },
+    greeting:{
+        fontSize:32,
+        color:colors.heading,
+        fontFamily:fonts.text
+    },
+    userName:{
+        fontSize:32,
+        fontFamily:fonts.heading,
+        color:colors.heading,
+        lineHeight:40
+    },
+    image:{
+        width:70,
+        height:70,
+        borderRadius:40
+    }
+})
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import colors from '../styles/colors'
+
 import { Welcome } from '../pages/Welcome'
 import { UserIdentification } from '../pages/UserIdentification'
 import { Confirmation } from '../pages/Confirmation'
+import { PlantSelect } from '../pages/PlantSelect'
 
 const stackRoutes = createStackNavigator()
 
@@ -27,6 +29,10 @@ const AppRoutes: React.FC = () => {
                 name='Confirmation'
                 component={Confirmation} />
 
+            <stackRoutes.Screen
+                name='PlantSelect'
+                component={PlantSelect} />
+            
         </stackRoutes.Navigator>
     )
 }
